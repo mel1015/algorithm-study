d3c191513c2e158984f8a520c35b7a071bc6b078
신광식 <sks1015sks@naver.com>
2017-08-15 16:05:11 +0900

    [신광식] 10845 풀이

---
+++ 10845/10845.cpp
@@ -0,0 +1,46 @@
+//
+// Created by sks10 on 2017-08-15.
+//
+#include <iostream>
+#include <queue>
+
+using namespace std;
+int n, a;
+queue<int> myqueue;
+string cmd;
+
+int main() {
+
+    for (cin >> n; n; n--) {
+        cin >> cmd;
+        if (cmd == "push") {
+            cin >> a;
+            myqueue.push(a);
+        } else if (cmd == "pop") {
+            if (myqueue.empty())
+                cout << -1 << endl;
+            else {
+                cout << myqueue.front() << endl;
+                myqueue.pop();
+            }
+        } else if (cmd == "size") {
+            cout << myqueue.size() << endl;
+        } else if (cmd == "empty") {
+            if (myqueue.empty())
+                cout << 1 << endl;
+            else
+                cout << 0 << endl;
+        } else if (cmd == "front") {
+            if (myqueue.empty())
+                cout << -1 << endl;
+            else
+                cout << myqueue.front() << endl;
+        } else if (cmd == "back") {
+            if (myqueue.empty())
+                cout << -1 << endl;
+            else
+                cout << myqueue.back() << endl;
+        }
+    }
+    return 0;
+}
