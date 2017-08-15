231c5e192bfa0198592da46c3697226924432291
신광식 <sks1015sks@naver.com>
2017-08-15 18:07:37 +0900

    [신광식] 10951 풀이

--- 10951/10951.cpp
+++ 10951/10951.cpp
@@ -2,3 +2,13 @@
 // Created by sks10 on 2017-08-15.
 //
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    while (cin >> a >> b)
+        cout << a + b << endl;
+    return 0;
+}
