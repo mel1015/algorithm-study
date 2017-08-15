1e433eb4fe8e714cc0508eb9e9c1c1ab7dca3c61
신광식 <sks1015sks@naver.com>
2017-08-15 15:16:32 +0900

    [신광식] 10950 풀이

---
+++ 10950/10950.cpp
@@ -0,0 +1,18 @@
+//
+// Created by sks10 on 2017-08-15.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t, a, b;
+
+    for ((cin >> t); t; t--) {
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+
+    return 0;
+}
