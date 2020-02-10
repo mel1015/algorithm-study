6d068b56df3ff1095e1b3a1529a06842c4a50b23
신광식 <sks1015sks@naver.com>
2020-02-10 22:34:17 +0900

    Sol200210 Maaaaaaaaaze

---
+++ working/src/solution/Main_백준_16985_Maaaaaaaaaze_200210.java
@@ -0,0 +1,155 @@
+package solution;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main_백준_16985_Maaaaaaaaaze_200210 {
+	static final int n = 5;
+	static int answer;
+	static int[][][] map, rotateMap;
+	static boolean[][][] visitMap;
+	static int[] order, rotate;
+	static boolean[] visit;
+	static int[] dx = { -1, 1, 0, 0, 0, 0 };
+	static int[] dy = { 0, 0, -1, 1, 0, 0 };
+	static int[] dz = { 0, 0, 0, 0, -1, 1 };
+
+	static void makeMap() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					rotateMap[i][j][k] = 1;
+				}
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			int idx = order[i];
+			int rotateCnt = rotate[i];
+
+			if (rotateCnt == 0) {
+				for (int x = 0; x < n; x++) {
+					for (int y = 0; y < n; y++) {
+						rotateMap[i][x][y] = map[idx][x][y];
+					}
+				}
+			} else if (rotateCnt == 1) {
+				for (int x = 0; x < n; x++) {
+					for (int y = 0; y < n; y++) {
+						if (map[idx][x][y] == 0) {
+							rotateMap[i][y][4 - x] = 0;
+						}
+					}
+				}
+			} else if (rotateCnt == 2) {
+				for (int x = 0; x < n; x++) {
+					for (int y = 0; y < n; y++) {
+						if (map[idx][x][y] == 0) {
+							rotateMap[i][4 - x][4 - y] = 0;
+						}
+					}
+				}
+			} else if (rotateCnt == 3) {
+				for (int x = 0; x < n; x++) {
+					for (int y = 0; y < n; y++) {
+						if (map[idx][x][y] == 0) {
+							rotateMap[i][4 - y][x] = 0;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	static int bfs(int pMap[][][]) {
+		Queue<int[]> q = new LinkedList<>();
+		q.offer(new int[] { 0, 0, 0, 0 });
+		visitMap[0][0][0] = true;
+
+		while (!q.isEmpty()) {
+			int x = q.peek()[0];
+			int y = q.peek()[1];
+			int z = q.peek()[2];
+			int cnt = q.poll()[3];
+			if (x == 4 && y == 4 && z == 4)
+				return cnt;
+			for (int d = 0; d < dx.length; d++) {
+				int nx = x + dx[d];
+				int ny = y + dy[d];
+				int nz = z + dz[d];
+				if (nx >= 0 && nx < n && ny >= 0 && ny < n && nz >= 0 && nz < n) {
+					if (pMap[nz][nx][ny] == 1 && !visitMap[nz][nx][ny]) {
+						visitMap[nz][nx][ny] = true;
+						q.offer(new int[] { nx, ny, nz, cnt + 1 });
+					}
+				}
+			}
+		}
+		return Integer.MAX_VALUE;
+	}
+
+	static void overlapPerm(int cnt) {
+		if (cnt == 5) {
+			makeMap();
+			if (rotateMap[0][0][0] == 1 && rotateMap[4][4][4] == 1) {
+				answer = Math.min(answer, bfs(rotateMap));
+				for (int i = 0; i < n; i++) {
+					for (int j = 0; j < n; j++) {
+						Arrays.fill(visitMap[i][j], false);
+						Arrays.fill(rotateMap[i][j], -1);
+					}
+				}
+			}
+			return;
+		}
+		for (int i = 0; i < 4; i++) {
+			rotate[cnt] = i;
+			overlapPerm(cnt + 1);
+		}
+	}
+
+	static void perm(int start, int cnt) {
+		if (cnt == 5) {
+			overlapPerm(0);
+			return;
+		}
+		for (int i = 0; i < n; i++) {
+			if (!visit[i]) {
+				visit[i] = true;
+				order[cnt] = i;
+				perm(i, cnt + 1);
+				visit[i] = false;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		map = new int[n][n][n];
+		rotateMap = new int[n][n][n];
+		visitMap = new boolean[n][n][n];
+		order = new int[n];
+		visit = new boolean[n];
+		rotate = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				st = new StringTokenizer(br.readLine());
+				for (int k = 0; k < n; k++) {
+					map[i][j][k] = Integer.parseInt(st.nextToken());
+					rotateMap[i][j][k] = -1;
+				}
+			}
+		}
+		answer = Integer.MAX_VALUE;
+		perm(0, 0);
+		System.out.println((answer == Integer.MAX_VALUE) ? -1 : answer);
+		br.close();
+	}
+
+}
