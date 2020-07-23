e1ee6a3b9556a6edb7115f81b618db8f422e4f7a
신광식 <sks1015sks@naver.com>
2020-07-23 14:41:35 +0900

    20.07.21 나머지 / 수학

---
+++ working/src/solution/Main_200721_백준_10430_나머지.java
@@ -0,0 +1,25 @@
+package solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_200721_백준_10430_나머지 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+
+		System.out.println((a + b) % c);
+		System.out.println(((a % c) + (b % c)) % c);
+		System.out.println((a * b) % c);
+		System.out.println(((a % c) * (b % c)) % c);
+
+		br.close();
+	}
+}
